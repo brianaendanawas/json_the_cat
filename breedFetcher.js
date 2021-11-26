1b3c05737c09f8013c64a956a03a394a4bda82df
Briana Endanawas <brianamay48@gmail.com>
2021-11-26 06:51:44 -0500

    updating

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
 
-let input = process.argv.slice(2);
-let breedName = input[0];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = function(breed, callback) {
 
-request(url, (error, response, body) => {
-  if (body === "[]") { // Edge Case: Breed Not Found
-    console.log("Breed not found.")
-  } else if (error !== null) { //Edge Case: Request Failed
-    console.log('error:', error);
-  } else { 
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(url, (error, response, body) => {
+    if (body === "[]") { // Edge Case: Breed Not Found
+      callback("Breed not found.", null);
+      return;
+    } else if (error) { //Edge Case: Request Failed
+      callback("URL not found." + error, null);
+      return;
+    } else { 
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+      return;
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
